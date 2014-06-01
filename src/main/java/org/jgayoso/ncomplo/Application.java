04745f2111c39924e71311ca84f4dacb39939be6
Javier Gayoso <jhavi85@gmail.com>
2014-06-01 22:11:30 +0200

    Datasource and JPA configuration added

--- src/main/java/org/jgayoso/ncomplo/Application.java
+++ src/main/java/org/jgayoso/ncomplo/Application.java
@@ -1,16 +1,39 @@
 package org.jgayoso.ncomplo;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-//@EnableJpaRepositories
+@EnableJpaRepositories
 public class Application {
 
+	
+//	@Bean
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
