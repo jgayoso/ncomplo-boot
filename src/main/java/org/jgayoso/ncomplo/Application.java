1e464ce9cef60855ae9a11434c083078a33fcad6
Javier Gayoso <jhavi85@gmail.com>
2014-06-04 00:06:13 +0200

    Jpa and datasource configured

--- src/main/java/org/jgayoso/ncomplo/Application.java
+++ src/main/java/org/jgayoso/ncomplo/Application.java
@@ -1,9 +1,6 @@
 package org.jgayoso.ncomplo;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.commons.dbcp.BasicDataSource;
+import org.jasypt.util.password.ConfigurablePasswordEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -18,21 +15,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Application {
 
 	
-//	@Bean
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+	@Bean
+	public ConfigurablePasswordEncryptor configurablePasswordEncryptor() {
+		return new ConfigurablePasswordEncryptor();
+	}
+	
 	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/org/jgayoso/ncomplo/web/controller/ScoreboardController.java
+++ src/main/java/org/jgayoso/ncomplo/web/controller/ScoreboardController.java
@@ -1,20 +1,208 @@
 package org.jgayoso.ncomplo.web.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jgayoso.ncomplo.business.entities.Bet;
+import org.jgayoso.ncomplo.business.entities.Bet.BetComparator;
+import org.jgayoso.ncomplo.business.entities.League;
+import org.jgayoso.ncomplo.business.entities.Round;
+import org.jgayoso.ncomplo.business.entities.User;
+import org.jgayoso.ncomplo.business.services.BetService;
+import org.jgayoso.ncomplo.business.services.LeagueService;
+import org.jgayoso.ncomplo.business.services.ScoreboardService;
+import org.jgayoso.ncomplo.business.services.UserService;
+import org.jgayoso.ncomplo.business.util.I18nNamedEntityComparator;
+import org.jgayoso.ncomplo.business.views.ScoreboardEntry;
+import org.jgayoso.ncomplo.exceptions.InternalErrorException;
+import org.jgayoso.ncomplo.web.beans.LeagueSelectorBean;
+import org.jgayoso.ncomplo.web.util.SessionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
-
-@RestController
+@Controller
 public class ScoreboardController {
+    
+    
+    @Autowired
+    private UserService userService;
 
-	
-	public ScoreboardController() {
-		super();
+    @Autowired
+    private BetService betService;
+
+    @Autowired
+    private LeagueService leagueService;
+
+    @Autowired
+    private ScoreboardService scoreboardService;
+    
+
+    
+    
+    public ScoreboardController() {
+        super();
     }
-	
-	@RequestMapping("/")
-	public String sayHello() {
-		return "Hello World";
-	}
-	
+    
+
+    
+    
+    
+    
+    @RequestMapping({"/","/scoreboard"})
+    public String scoreboard(
+            final HttpServletRequest request) {
+        
+        final Locale locale = RequestContextUtils.getLocale(request);
+        
+        final String login = SessionUtil.getAuthenticatedUser(request);
+        final User user = this.userService.find(login);
+        
+        final List<League> userLeagues = new ArrayList<League>(user.getLeagues());
+        final List<League> activeUserLeagues = new ArrayList<League>();
+        for (final League league : userLeagues) {
+            if (league.isActive()) {
+                activeUserLeagues.add(league);
+            }
+        }
+        Collections.sort(activeUserLeagues, new I18nNamedEntityComparator(locale));
+        
+        if (activeUserLeagues.size() == 0) {
+            throw new InternalErrorException("No active leagues for user \"" +  login + "\"");
+        }
+
+        return "redirect:/scoreboard/" + activeUserLeagues.get(0).getId();
+        
+    }
+    
+
+
+    
+    
+    @RequestMapping("/scoreboard/{leagueId}")
+    public String scoreboardByLeague(
+            @PathVariable("leagueId") final Integer leagueId,
+            final HttpServletRequest request,
+            final ModelMap model) {
+        
+        return computeScoreboard(leagueId, null, request, model);
+        
+    }
+    
+    
+    
+
+    @RequestMapping("/scoreboard/{leagueId}/{roundId}")
+    public String scoreboardByLeagueAndRound(
+            @PathVariable("leagueId") final Integer leagueId,
+            @PathVariable("roundId") final Integer roundId,
+            final HttpServletRequest request,
+            final ModelMap model) {
+        
+        return computeScoreboard(leagueId, roundId, request, model);
+        
+    }
+    
+    
+    
+
+    private String computeScoreboard(
+            final Integer leagueId,
+            final Integer roundId,
+            final HttpServletRequest request,
+            final ModelMap model) {
+        
+        final Locale locale = RequestContextUtils.getLocale(request);
+        
+        final String login = SessionUtil.getAuthenticatedUser(request);
+        final User user = this.userService.find(login);
+        
+        final List<League> userLeagues = new ArrayList<League>(user.getLeagues());
+        final List<League> activeUserLeagues = new ArrayList<League>();
+        for (final League league : userLeagues) {
+            if (league.isActive()) {
+                activeUserLeagues.add(league);
+            }
+        }
+        Collections.sort(activeUserLeagues, new I18nNamedEntityComparator(locale));
+
+        final LeagueSelectorBean leagueSelectorBean = new LeagueSelectorBean();
+        leagueSelectorBean.setLeagueId(leagueId);
+        leagueSelectorBean.setRoundId(roundId);
+
+        final League league = this.leagueService.find(leagueId);
+        
+        final List<Round> rounds = new ArrayList<Round>(league.getCompetition().getRounds());
+        Collections.sort(rounds);
+        
+        final List<ScoreboardEntry> scoreboardEntries =
+                this.scoreboardService.computeScoreboard(leagueId, roundId, locale);
+       
+        model.addAttribute("scoreboardEntries", scoreboardEntries);
+        model.addAttribute("user", user);
+        model.addAttribute("league", league);
+        model.addAttribute("leagueSelector", leagueSelectorBean);
+        model.addAttribute("allLeagues", activeUserLeagues);
+        model.addAttribute("allRounds", rounds);
+        model.addAttribute("showLeagueSelector", Boolean.valueOf(activeUserLeagues.size() > 1));
+        
+        return "scoreboard";
+        
+    }
+    
+    
+    
+
+    @RequestMapping({"/selectScoreboard"})
+    public String selectScoreboard(final LeagueSelectorBean leagueSelectorBean) {
+        final Integer leagueId = leagueSelectorBean.getLeagueId();
+        final Integer roundId = leagueSelectorBean.getRoundId();
+        if (roundId != null) {
+            return "redirect:/scoreboard/" + leagueId + "/" + roundId;
+        }
+        return "redirect:/scoreboard/" + leagueId;
+    }
+
+    
+    
+
+    
+    @RequestMapping({"/bets/{leagueId}/{login}"})
+    public String bets(
+            @PathVariable("leagueId") Integer leagueId,
+            @PathVariable("login") String login,
+            final HttpServletRequest request,
+            final ModelMap model) {
+        
+        final Locale locale = RequestContextUtils.getLocale(request);
+        
+        final User user = this.userService.find(login);
+        final List<Bet> betsForUser =
+                this.betService.findByLeagueIdAndUserLogin(leagueId, login, locale);
+        Collections.sort(betsForUser, new BetComparator(locale));
+        
+        final League league = this.leagueService.find(leagueId);
+        
+        final List<ScoreboardEntry> scoreboardEntries =
+                this.scoreboardService.computeScoreboard(leagueId, null, locale);
+       
+        model.addAttribute("scoreboardEntries", scoreboardEntries);
+        
+        model.addAttribute("user", user);
+        model.addAttribute("league", league);
+        model.addAttribute("allBets", betsForUser);
+        
+        return "bets";
+        
+    }
+    
+    
+    
 }
