2031574952ad226bd77d5afe78ee1f19285b8921
Oscar del Pozo <oscardelpozog@gmail.com>
2016-06-05 12:30:21 +0200

    Added deadline date to League entity

--- src/main/java/org/jgayoso/ncomplo/business/entities/League.java
+++ src/main/java/org/jgayoso/ncomplo/business/entities/League.java
@@ -1,5 +1,6 @@
 package org.jgayoso.ncomplo.business.entities;
 
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Locale;
@@ -25,125 +26,108 @@ import javax.persistence.Table;
 
 import org.jgayoso.ncomplo.business.util.I18nUtils;
 
-
 @Entity
-@Table(name="LEAGUE")
+@Table(name = "LEAGUE")
 public class League implements I18nNamedEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-    
-    
-    @Column(name="NAME",nullable=false,length=200)
-    private String name;
-    
-    
-    @ElementCollection(fetch=FetchType.EAGER,targetClass=java.lang.String.class)
-    @CollectionTable(name="LEAGUE_NAME_I18N",joinColumns=@JoinColumn(name="LEAGUE_ID"))
-    @MapKeyColumn(name="LANG",nullable=false,length=20)
-    @Column(name="NAME", nullable=false,length=200)
-    private final Map<String,String> namesByLang = new LinkedHashMap<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 
-    
-    @Column(name="ACTIVE",nullable=false)
-    private boolean active = true;
-    
-    
-    @ManyToOne
-    @JoinColumn(name="COMPETITION_ID",nullable=false)
-    private Competition competition; 
+	@Column(name = "NAME", nullable = false, length = 200)
+	private String name;
 
-    
-    @Column(name="ADMIN_EMAIL",nullable=false)
-    private String adminEmail;
+	@ElementCollection(fetch = FetchType.EAGER, targetClass = java.lang.String.class)
+	@CollectionTable(name = "LEAGUE_NAME_I18N", joinColumns = @JoinColumn(name = "LEAGUE_ID") )
+	@MapKeyColumn(name = "LANG", nullable = false, length = 20)
+	@Column(name = "NAME", nullable = false, length = 200)
+	private final Map<String, String> namesByLang = new LinkedHashMap<>();
 
-    
-    @ManyToMany
-    private final Set<User> participants = new LinkedHashSet<>();
+	@Column(name = "ACTIVE", nullable = false)
+	private boolean active = true;
 
-    
-    @OneToMany(cascade=CascadeType.ALL,orphanRemoval=true,mappedBy="league")
-    @MapKey(name="game")
-    private final Map<Game,LeagueGame> leagueGames = new LinkedHashMap<>();
+	@ManyToOne
+	@JoinColumn(name = "COMPETITION_ID", nullable = false)
+	private Competition competition;
 
-    
-    
-    
-    public League() {
-        super();
-    }
+	@Column(name = "ADMIN_EMAIL", nullable = false)
+	private String adminEmail;
 
+	@Column(name = "BETS_DEADLINE_TIME", nullable = false)
+	private Date betsDeadLine;
 
-    
+	@ManyToMany
+	private final Set<User> participants = new LinkedHashSet<>();
 
-    @Override
-    public String getName() {
-        return this.name;
-    }
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "league")
+	@MapKey(name = "game")
+	private final Map<Game, LeagueGame> leagueGames = new LinkedHashMap<>();
 
+	public League() {
+		super();
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	@Override
+	public String getName() {
+		return this.name;
+	}
 
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    public boolean isActive() {
-        return this.active;
-    }
+	public boolean isActive() {
+		return this.active;
+	}
 
+	public void setActive(final boolean active) {
+		this.active = active;
+	}
 
-    public void setActive(final boolean active) {
-        this.active = active;
-    }
+	public Competition getCompetition() {
+		return this.competition;
+	}
 
+	public void setCompetition(final Competition competition) {
+		this.competition = competition;
+	}
 
-    public Competition getCompetition() {
-        return this.competition;
-    }
+	public String getAdminEmail() {
+		return this.adminEmail;
+	}
 
+	public void setAdminEmail(final String adminEmail) {
+		this.adminEmail = adminEmail;
+	}
 
-    public void setCompetition(final Competition competition) {
-        this.competition = competition;
-    }
+	public Integer getId() {
+		return this.id;
+	}
 
+	@Override
+	public Map<String, String> getNamesByLang() {
+		return this.namesByLang;
+	}
 
-    public String getAdminEmail() {
-        return this.adminEmail;
-    }
+	public Set<User> getParticipants() {
+		return this.participants;
+	}
 
+	public Date getBetsDeadLine() {
+		return betsDeadLine;
+	}
 
-    public void setAdminEmail(final String adminEmail) {
-        this.adminEmail = adminEmail;
-    }
+	public void setBetsDeadLine(Date betsDeadLine) {
+		this.betsDeadLine = betsDeadLine;
+	}
 
+	@Override
+	public String getName(final Locale locale) {
+		return I18nUtils.getTextForLocale(locale, this.namesByLang, this.name);
+	}
 
-    public Integer getId() {
-        return this.id;
-    }
+	public Map<Game, LeagueGame> getLeagueGames() {
+		return this.leagueGames;
+	}
 
-
-    @Override
-    public Map<String, String> getNamesByLang() {
-        return this.namesByLang;
-    }
-
-
-    public Set<User> getParticipants() {
-        return this.participants;
-    }
-
-
-    @Override
-    public String getName(final Locale locale) {
-        return I18nUtils.getTextForLocale(locale, this.namesByLang, this.name);
-    }
-
-    
-    public Map<Game,LeagueGame> getLeagueGames() {
-        return this.leagueGames;
-    }
-    
-    
-    
 }
--- src/main/java/org/jgayoso/ncomplo/web/admin/beans/LeagueBean.java
+++ src/main/java/org/jgayoso/ncomplo/web/admin/beans/LeagueBean.java
@@ -2,6 +2,7 @@ package org.jgayoso.ncomplo.web.admin.beans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,104 +11,92 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-
 public class LeagueBean implements Serializable {
-    
-    private static final long serialVersionUID = 2981813244600985282L;
 
-    
-    @NotNull
-    private Integer id;
-    
-    @NotNull
-    private Integer competitionId;
-    
-    @NotNull
-    @Length(min=3,max=200)
-    private String name;
-    
-    @NotNull
-    private final List<LangBean> namesByLang = new ArrayList<>();
+	private static final long serialVersionUID = 2981813244600985282L;
 
-    @NotNull
-    private boolean active = true;
+	@NotNull
+	private Integer id;
 
-    @NotNull
-    private String adminEmail;
+	@NotNull
+	private Integer competitionId;
 
-    
-    @NotNull
-    private final Map<Integer,Integer> betTypesByGame = new LinkedHashMap<>();
-    
-    
-    
-    public LeagueBean() {
-        super();
-    }
+	@NotNull
+	@Length(min = 3, max = 200)
+	private String name;
 
+	@NotNull
+	private final List<LangBean> namesByLang = new ArrayList<>();
 
-    
+	@NotNull
+	private boolean active = true;
 
-    public Integer getId() {
-        return this.id;
-    }
+	@NotNull
+	private String adminEmail;
 
+	private String date;
 
-    public void setId(final Integer id) {
-        this.id = id;
-    }
+	@NotNull
+	private final Map<Integer, Integer> betTypesByGame = new LinkedHashMap<>();
 
+	public LeagueBean() {
+		super();
+	}
 
-    public Integer getCompetitionId() {
-        return this.competitionId;
-    }
+	public Integer getId() {
+		return this.id;
+	}
 
+	public void setId(final Integer id) {
+		this.id = id;
+	}
 
-    public void setCompetitionId(final Integer competitionId) {
-        this.competitionId = competitionId;
-    }
+	public Integer getCompetitionId() {
+		return this.competitionId;
+	}
 
+	public void setCompetitionId(final Integer competitionId) {
+		this.competitionId = competitionId;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public String getName() {
+		return this.name;
+	}
 
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public boolean isActive() {
+		return this.active;
+	}
 
+	public void setActive(final boolean active) {
+		this.active = active;
+	}
 
-    public boolean isActive() {
-        return this.active;
-    }
+	public String getAdminEmail() {
+		return this.adminEmail;
+	}
 
+	public void setAdminEmail(final String adminEmail) {
+		this.adminEmail = adminEmail;
+	}
 
-    public void setActive(final boolean active) {
-        this.active = active;
-    }
+	public List<LangBean> getNamesByLang() {
+		return this.namesByLang;
+	}
 
+	public Map<Integer, Integer> getBetTypesByGame() {
+		return this.betTypesByGame;
+	}
 
-    public String getAdminEmail() {
-        return this.adminEmail;
-    }
+	public String getDate() {
+		return this.date;
+	}
 
+	public void setDate(String date) {
+		this.date = date;
+	}
 
-    public void setAdminEmail(final String adminEmail) {
-        this.adminEmail = adminEmail;
-    }
-
-
-    public List<LangBean> getNamesByLang() {
-        return this.namesByLang;
-    }
-
-
-    public Map<Integer, Integer> getBetTypesByGame() {
-        return this.betTypesByGame;
-    }
-
-
-    
-    
 }
